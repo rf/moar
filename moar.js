511704f0edcc011fdd414af8f35f121a26d02ce2
Russell Frank <russ.frank@rutgers.edu>
2012-05-09 23:06:08 -0400

    supporting simple searching; bin/moar

--- moar.js
+++ moar.js
@@ -3,6 +3,7 @@ var util = require('utile');
 var wordwrap = require('wordwrap');
 var _ = require('underscore');
 var tty = require('tty');
+var colors = require('colors');
 var EventEmitter = require('events').EventEmitter;
 
 var size = process.stdout.getWindowSize();
@@ -14,24 +15,36 @@ var wrap = wordwrap(width, {mode: 'hard'});
 module.exports = function (options) {
   var exports = new EventEmitter();
   var buffer = [];
+  var searchResults = [];
   var previousStart = 0;
+  var commandBufferLength = 0;
   var start = 0;
-  var stdin;
-  var searchBuffer = '';
+  var commandBuffer = '';
   var searchDirection;
   var visible = 0;
-
   var mode = 'command';
 
   term.clear();
+
+  // actually run the search
+  function search () {
+    var regex = new RegExp(commandBuffer);
+    for (var i = start; i < start + height; i++) {
+      if (!searchResults[i] && buffer[i] !== undefined) {
+        searchResults[i] = buffer[i].match(regex);
+      }
+    }
+  }
 
   function refresh () {
     _.each(buffer.slice(start, start + height), function (item, index) {
         term
           .move(index, 0)
           .clearCharacters(width)
-          .move(index, 0)
-          .write(item);
+          .move(index, 0);
+
+        if (searchResults[index + start]) term.write(item.blue);
+        else term.write(item);
     });
 
     drawPrompt();
@@ -45,7 +58,7 @@ module.exports = function (options) {
         .move(height, 0)
         .clearCharacters(width)
         .move(height, 0)
-        .write(':');
+        .write(':' + commandBuffer);
     }
 
     if (mode == 'search' || mode == 'searchExecute') {
@@ -53,11 +66,13 @@ module.exports = function (options) {
         .move(height, 0)
         .clearCharacters(width)
         .move(height, 0)
-        .write(searchDirection + searchBuffer);
+        .write(searchDirection + commandBuffer);
     }
   }
 
   function write (data) {
+    // if we're given a buffer
+    if (data.isBuffer) data = data.toString('utf8');
     buffer = buffer.concat((wrap(data).split('\n')));
     if (visible < height) refresh();
   }
@@ -71,6 +86,9 @@ module.exports = function (options) {
       if (key.name == 'pageup') start -= height;
       if (key.name == 'pagedown' || (chunk == ' ' && mode != 'search')) start += height;
 
+      if (key.name == 'j') start += 1;
+      if (key.name == 'k') start -= 1;
+
       if (key.name == 'q' || (key.ctrl && key.name == 'c')) {
         tty.setRawMode(false);
         term.clear();
@@ -80,57 +98,68 @@ module.exports = function (options) {
 
       if (key.name == 'escape') {
         mode = 'command';
-        searchBuffer = '';
+        commandBuffer = '';
       }
 
       if (key.name == 'backspace') {
-        searchBuffer = searchBuffer.slice(0, searchBuffer.length - 2);
+        commandBuffer = commandBuffer.slice(0, commandBuffer.length - 1);
       }
 
       if (key.name == 'enter') {
         if (mode == 'search') {
           mode = 'searchExecute';
+          search();
+          refresh();
         } 
       }
     }
 
-    if (mode == 'search' && chunk) {
-      searchBuffer += chunk;
+    if (mode == 'search' && (key && key.name != 'backspace') && chunk !== undefined) {
+      commandBuffer += chunk;
     }
 
     if (chunk == '/') {
-      if (mode == 'command') {
+      if (mode != 'search') {
         mode = 'search';
         searchDirection = '/';
+        commandBuffer = '';
+        searchResults = [];
       }
     }
 
     if (chunk == '?') {
-      if (mode == 'command') {
+      if (mode != 'search') {
         mode = 'search';
         searchDirection = '?';
+        commandBuffer = '';
+        searchResults = [];
       }
     }
 
     if (start < 0) start = 0;
     if (start > buffer.length - height) start = buffer.length - height;
 
-    if (start != previousStart) 
-      refresh();
+    if (mode == 'searchExecute') search();
+
+    if (start != previousStart) refresh();
 
     drawPrompt();
 
     previousStart = start;
   }
 
+  function end () { /* nop */ }
+
   process.nextTick(function () {
-    stdin = process.openStdin();
+    process.openStdin();
     process.stdin.on('keypress', listener);
     tty.setRawMode(true);
   });
 
   exports.write = write;
   exports.refresh = refresh;
+  exports.end = end;
+  exports.writable = true;
 
   process.on('exit', function () { tty.setRawMode(false); });
 
