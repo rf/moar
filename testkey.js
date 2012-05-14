c1856aaf4cb477475aab4395e8434909bfd4f9ce
Russ Frank <russ.frank@rutgers.edu>
2012-05-14 13:13:52 -0400

    removing test file

--- testkey.js
+++
@@ -1,9 +0,0 @@
-var tty = require('tty');
-
-process.openStdin();
-tty.setRawMode(true);
-process.stdin.on('keypress', function (chunk, key) {
-  console.dir(chunk);
-  console.dir(key);
-  if (key && key.name == 'q') { tty.setRawMode(false); process.exit(); }
-});
