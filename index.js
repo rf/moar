1d88f655ec8eec7b0bfba943136aab2ee4866055
Russ Frank <russ.frank@rutgers.edu>
2012-05-23 16:35:43 -0400

    changing interface to allow options

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ module.exports = (function () {
 
   // for windows, use the javascript version
   if (process.platform == "win32") return require('./moar')();
+  else return function (options) {
 
-  var spawn = require('child_process').spawn;
-  var pager;
+    var spawn = require('child_process').spawn;
+    var pager;
 
-  if (process.env.PAGER) {
-    pager = process.env.PAGER;
-  } else {
-    pager = 'less';
-  }
+    if (process.env.PAGER) {
+      pager = process.env.PAGER;
+    } else {
+      pager = 'less';
+    }
 
-  // less needs -F to handle ansi color escapes properly
-  if (pager == 'less') args = ['-R']; 
+    // less needs -F to handle ansi color escapes properly
+    if (pager == 'less') args = ['-R']; 
 
-  var ps = spawn(pager, args, {customFds: [-1, 1, 2]}); 
+    var ps = spawn(pager, args, {customFds: [-1, 1, 2]}); 
 
-  ps.on('exit', function () {
-    ps.stdin.emit('done');
-  });
+    ps.on('exit', function () {
+      ps.stdin.emit('done');
+    });
 
-  return ps.stdin;
+    return ps.stdin;
+
+  };
 
 }());
 
