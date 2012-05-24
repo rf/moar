22a19094388cceae5f2dbeb318749c99578ee892
Russell Frank <russ.frank@rutgers.edu>
2012-05-23 22:57:04 -0400

    fixing examples

--- example.js
+++ example.js
@@ -1,14 +1,8 @@
-var moar = require('./index');
+var moar = require('./index')();
 
 moar.write('hello');
 
-var r1 = require('request')('http://nodejs.org').pipe(moar);
-var r2 = require('request')('http://google.org').pipe(moar);
-
-require('async').parallel([
-  function (callback) { r1.on('end', callback); },
-  function (callback) { r2.on('end', callback); },
-], moar.end);
-
+var req = require('request')('http://nodejs.org').pipe(moar);
+req.on('end', moar.end);
 moar.on('done', process.exit);
 
--- example2.js
+++ example2.js
@@ -1,3 +1,3 @@
-var moar = require('./index');
+var moar = require('./index')();
 
 require('request')('http://nodejs.org').pipe(moar).on('end', moar.end);
