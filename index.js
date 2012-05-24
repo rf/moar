679761e4ab251bd2653785f77660a17f3372c691
Russell Frank <russ.frank@rutgers.edu>
2012-05-23 20:31:35 -0400

    fixing handling of $PAGER

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = (function () {
 
     var spawn = require('child_process').spawn;
     var pager;
+    var args = [];
 
     if (process.env.PAGER) {
       pager = process.env.PAGER;
@@ -13,7 +14,7 @@ module.exports = (function () {
       pager = 'less';
     }
 
-    // less needs -F to handle ansi color escapes properly
+    // less needs -R to handle ansi color escapes properly
     if (pager == 'less') args = ['-R']; 
 
     var ps = spawn(pager, args, {customFds: [-1, 1, 2]}); 
